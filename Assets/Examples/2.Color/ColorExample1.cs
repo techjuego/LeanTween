920b25917ecde8322c07ce7397714476ca469a00
Mayur <mayur.saitwal@gmail.com>
2024-07-17 22:08:05 +0530

    color

---
+++ Assets/Examples/2.Color/ColorExample1.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorExample1 : MonoBehaviour
+{
+    public GameObject objectToPerform;
+    public float time;
+    public Color changeColor;
+    private void OnEnable()
+    {
+        LeanTween.color(objectToPerform, changeColor, time);
+    }
+}
